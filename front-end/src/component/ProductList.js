286187c529c42e6f3816832254951e26d7baae46
SHIVAM DESHWAL <shivamdeshwal1414@gmail.com>
2023-09-20 17:18:17 +0530

    ui updatedd

--- front-end/src/component/ProductList.js
+++ front-end/src/component/ProductList.js
@@ -32,16 +32,16 @@ const ProductList = () => {
   return (
     <>
     
-      <div id="carouselExampleCaptions" class="carousel slide " data-bs-ride="carousel">
-  <div class="carousel-indicators">
+    <div id="carouselExampleCaptions" class="carousel slide " data-bs-ride="carousel">
+    <div class="carousel-indicators">
     <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
     <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
     <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
    
   </div>
-  <div class="carousel-inner ">
-    <div class="carousel-item active" data-bs-interval="5000">
-      <img src={odop} class="d-block w-100 carousel-img" alt="..."/>
+  <div class="carousel-inner "  style={{padding:'50px'}}> 
+    <div class="carousel-item active" data-bs-interval="5000"  >
+      <img src={odop} class="d-block w-100 carousel-img " alt="..."/>
       <div class="carousel-caption d-none d-md-block">
         
       </div>
